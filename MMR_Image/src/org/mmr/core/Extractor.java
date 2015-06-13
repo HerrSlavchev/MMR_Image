7e0603ec39431a795ccf43dbf1e7bf8c8d769f28
root <root@srvr1>
2015-06-14 00:44:16 +0200

    Fuzzifier faster than a frenetic ferret!

--- MMR_Image/src/org/mmr/core/Extractor.java
+++ MMR_Image/src/org/mmr/core/Extractor.java
@@ -14,70 +14,70 @@ import org.apache.tika.Tika;
  */
 public class Extractor {
 
-	public static Optional<Document> extract(final Path file, final boolean fromAllowedContentType) throws IOException {
-		final String detectedValue = new Tika().detect(file.toFile());
-		final Optional<EContentType> contentType = EContentType.of(detectedValue);
+    public static Optional<Document> extract(final Path file, final boolean fromAllowedContentType) throws IOException {
+        final String detectedValue = new Tika().detect(file.toFile());
+        final Optional<EContentType> contentType = EContentType.of(detectedValue);
 
-		if (contentType.isPresent()) {
-			if (fromAllowedContentType && !Context.getAllowedContentTypes().contains(contentType.get())) {
-				return Optional.empty();
-			}
+        if (contentType.isPresent()) {
+            if (fromAllowedContentType && !Context.getAllowedContentTypes().contains(contentType.get())) {
+                return Optional.empty();
+            }
 
-			return Optional.of(createDocumentBean(file));
-		} else {
-			return Optional.empty();
-		}
-	}
+            return Optional.of(createDocumentBean(file));
+        } else {
+            return Optional.empty();
+        }
+    }
 
-	private static Document createDocumentBean(final Path file) throws IOException {
-		final int bins = Context.getHistogramBinCount();
+    private static Document createDocumentBean(final Path file) throws IOException {
+        final int bins = Context.getHistogramBinCount();
 
-		final String absolutePath = file.toAbsolutePath().toString();
+        final String absolutePath = file.toAbsolutePath().toString();
 
-		final BufferedImage bufferedImage = ImageIO.read(file.toFile());
-		final Raster raster = bufferedImage.getRaster();
-		final int width = raster.getWidth();
-		final int height = raster.getHeight();
+        final BufferedImage bufferedImage = ImageIO.read(file.toFile());
+        final Raster raster = bufferedImage.getRaster();
+        final int width = raster.getWidth();
+        final int height = raster.getHeight();
 
-		//RGB histogram components
-		final float[] red = new float[bins];
-		final float[] green = new float[bins];
-		final float[] blue = new float[bins];
+        //RGB histogram components
+        final float[] red = new float[bins];
+        final float[] green = new float[bins];
+        final float[] blue = new float[bins];
 
-		//HSB histogram components
-		final float[] hue = new float[bins];
-		final float[] saturation = new float[bins];
-		final float[] brightness = new float[bins];
+        //HSB histogram components
+        final float[] hue = new float[bins];
+        final float[] saturation = new float[bins];
+        final float[] brightness = new float[bins];
 
-		float[] tmpValues;
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
+        float[] tmpValues;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
 
-				final int[] rgb = new int[4];
-				raster.getPixel(x, y, rgb);
-				tmpValues = Fuzzyfier.extractValuesForIndex(rgb[0], 0, 255, bins);
-				Fuzzyfier.updateHistogram(tmpValues, red);
-				tmpValues = Fuzzyfier.extractValuesForIndex(rgb[1], 0, 255, bins);
-				Fuzzyfier.updateHistogram(tmpValues, green);
-				tmpValues = Fuzzyfier.extractValuesForIndex(rgb[2], 0, 255, bins);
-				Fuzzyfier.updateHistogram(tmpValues, blue);
+                final int[] rgb = new int[4];
+                raster.getPixel(x, y, rgb);
+                tmpValues = Fuzzyfier.extractValuesForIndexRGB(rgb[0], 0, 255, bins);
+                Fuzzyfier.updateHistogram(tmpValues, red);
+                tmpValues = Fuzzyfier.extractValuesForIndexRGB(rgb[1], 0, 255, bins);
+                Fuzzyfier.updateHistogram(tmpValues, green);
+                tmpValues = Fuzzyfier.extractValuesForIndexRGB(rgb[2], 0, 255, bins);
+                Fuzzyfier.updateHistogram(tmpValues, blue);
 
-				final float[] hsb = new float[3];
-				Color.RGBtoHSB(rgb[0], rgb[1], rgb[2], hsb);
-				tmpValues = Fuzzyfier.extractValuesForIndex(hsb[0], 0, 1, bins);
-				Fuzzyfier.updateHistogram(tmpValues, hue);
-				tmpValues = Fuzzyfier.extractValuesForIndex(hsb[1], 0, 1, bins);
-				Fuzzyfier.updateHistogram(tmpValues, saturation);
-				tmpValues = Fuzzyfier.extractValuesForIndex(hsb[2], 0, 1, bins);
-				Fuzzyfier.updateHistogram(tmpValues, brightness);
-			}
-		}
+                final float[] hsb = new float[3];
+                Color.RGBtoHSB(rgb[0], rgb[1], rgb[2], hsb);
+                tmpValues = Fuzzyfier.extractValuesForIndexHSB(hsb[0], 0, 1, bins);
+                Fuzzyfier.updateHistogram(tmpValues, hue);
+                tmpValues = Fuzzyfier.extractValuesForIndexHSB(hsb[1], 0, 1, bins);
+                Fuzzyfier.updateHistogram(tmpValues, saturation);
+                tmpValues = Fuzzyfier.extractValuesForIndexHSB(hsb[2], 0, 1, bins);
+                Fuzzyfier.updateHistogram(tmpValues, brightness);
+            }
+        }
 
-		final float[][] histogramHSB = new float[3][];
-		histogramHSB[0] = hue;
-		histogramHSB[1] = saturation;
-		histogramHSB[2] = brightness;
+        final float[][] histogramHSB = new float[][]{
+            hue,
+            saturation,
+            brightness,};
 
-		return new Document(absolutePath, width, height, histogramHSB);
-	}
+        return new Document(absolutePath, width, height, histogramHSB);
+    }
 }
--- MMR_Image/src/org/mmr/core/Fuzzyfier.java
+++ MMR_Image/src/org/mmr/core/Fuzzyfier.java
@@ -5,45 +5,95 @@
  */
 package org.mmr.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author root
  */
 public class Fuzzyfier {
 
-    public static float[] toHistogram(float[] feed, float normalizationMinimum, float normalizationMaximum, int bins) {
-
-        float[] res = new float[bins];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = 0;
+    private static final Map<Float, float[]> rgbCache = new HashMap();
+    private static final Map<Float, float[]> hsbCache = new HashMap();
+        
+    public static float[] extractValuesForIndexRGB(float value, float normalizationMinimum, float normalizationMaximum, int bins) {
+        
+        float[] res;
+        //use caching in order to lookup known results instead of repeating all computations
+        float[] cached = rgbCache.get(value);
+        if(cached != null) {
+            res = cached;
+        } else {
+            res = extractValuesForIndex(value, normalizationMinimum, normalizationMaximum, bins);
+            rgbCache.put(value, res);
         }
-
-        for (int i = 0; i < feed.length; i++) {
-            float value = feed[i];
-            float[] valuesAndIndex = extractValuesForIndex(value, normalizationMinimum, normalizationMaximum, bins);
-            updateHistogram(valuesAndIndex, res);
+        
+        return res;
+    }
+    
+    public static float[] extractValuesForIndexHSB(float value, float normalizationMinimum, float normalizationMaximum, int bins) {
+        
+        float[] res;
+        //use caching in order to lookup known results instead of repeating all computations
+        float[] cached = hsbCache.get(value);
+        if(cached != null) {
+            res = cached;
+        } else {
+            res = extractValuesForIndex(value, normalizationMinimum, normalizationMaximum, bins);
+            hsbCache.put(value, res);
         }
-
+        
         return res;
     }
 
-    public static float[] extractValuesForIndex(float value, float normalizationMinimum, float normalizationMaximum, int bins) {
-
+    /**
+     * @param value - a simple scalar value that will be separated between the two closest bins
+     * @param normalizationMinimum - the minimal value of the expected data region
+     * @param normalizationMaximum - the maximal value of the expected data region
+     * @param bins - the number of bins in which the region must be separated
+     * @return - array with three values: 
+     *      v[0] = index position of the first bin, 
+     *      v[1] = how much should be added to the first bin, 
+     *      v[2] = how much should be added to the next bin (set to 0 if v[0] is index of the last possible bin)
+     */
+    private static float[] extractValuesForIndex(float value, float normalizationMinimum, float normalizationMaximum, int bins) {
+                
         float[] res = new float[]{
             0, //the index position of preceeding bin
-            0, //the value to add to "preceeding" bin
-            0 //the value to add to "following" bin -- use only if different from 0, otherwise ignore!
+            0, //the value to add to preceeding bin
+            0 //the value to add to following bin -- use only if different from 0, otherwise ignore!
         };
 
-        float binLength = (normalizationMaximum - normalizationMinimum) / bins;
+        /*e.g: we expect values from 0 to 255 and want to separate them into 32 bins -> check how large each bin is
+         1) from 0 to 255 -> (255 - 0 + 1) = 256 possible discrete values
+         2) 32 bins, each will cover 256 / 32 = 8 discrete values
+         */
+        float binLength = (normalizationMaximum - normalizationMinimum + 1) / bins;
+        /*e.g: we want to determine between which bins is the given value, e.g 57
+        relativeIdx = 57 / 8 = 7.125
+        resIdx = 7 -> the given value is between bins with index 7 and 8
+        */
         float relativeIdx = value / binLength;
         int resIdx = (int) relativeIdx;
 
-        res[0] = resIdx + 0.0001f;
+        /*
+        store the preceeding index in the first field of the result
+        */
+        res[0] = resIdx;
+        
+        /*
+        1) if the given value is precisely equal to a bin, e.g value = 64 
+                -> equal to bins[8] -> attribute 1 to bins[8] and 0 to bins[9]
+        2) if the given value is from the last bin, e.g value = 250 
+                -> attribute 1 to bins[last] and set res[2] = 0 to mark that there is no next bin
+        3) else: determine distance between value and surrounding bins in order to decide how much should be attributed to each bin
+                (hint: use distance between the relative and discrete index to obtain it directly)
+        */
         if (value % bins == 0) {
             res[1] = 1;
-        } else if (resIdx >= res.length - 1) {
-            res[0] = res.length - 0.9999f;
+        } else if (resIdx >= bins - 1) {
+            res[0] = bins - 1;
             res[1] = 1;
         } else {
             float distToLowerBin = relativeIdx - resIdx;
@@ -51,6 +101,7 @@ public class Fuzzyfier {
             res[1] = distToUpperBin;
             res[2] += distToLowerBin;
         }
+        
         return res;
     }
 
